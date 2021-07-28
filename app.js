6ec8c9e009cd181b9d9f7e11640a5a10188baf94
Matheus silva <teus_26@hotmail.com>
2021-07-28 13:47:43 -0300

    middleware, flash and session

--- app.js
+++ app.js
@@ -5,7 +5,27 @@ const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
-//const mongoose = require("mongoose")
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
+
+//configuration
+//session
+app.use(
+  session({
+    secret: 'cursodenode',
+    resave: true,
+    saveUninitialized: true
+  })
+)
+app.use(flash())
+
+// middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 //body parser
 app.use(express.urlencoded({ extended: true }))
@@ -14,9 +34,32 @@ app.use(express.json())
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //mongoose
+mongoose.Promise = global.Promise
+mongoose
+  .connect('mongodb://localhost/blogapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Conectado com sucesso!')
+  })
+  .catch(err => {
+    mongoose.Promise = global.Promise
+    console.log('Erro ao conectar: ' + err)
+  })
 //public
 app.use(express.static(path.join(__dirname, 'public')))
+
 //rotas
+
+app.get('/', (req, res) => {
+  res.send('Rota principal')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('Lista de posts')
+})
+
 app.use('/admin', admin)
 
 //outros
