67a1786a217c8d4a802758e2120b771d0cda8b36
Matheus silva <teus_26@hotmail.com>
2021-07-28 13:48:52 -0300

    form validation

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
 
 // rota principal para o painel administrativo
 router.get('/', (req, res) => {
@@ -19,6 +22,42 @@ router.get('/categorias', (req, res) => {
 // rota para o formulário
 router.get('/categorias/add', (req, res) => {
   res.render('admin/addcategorias')
+})
+
+// rota responsável por cadastrar o formulário dentro do mongo
+router.post('/categorias/nova', (req, res) => {
+  var erros = []
+
+  if (!req.body.nome) {
+    erros.push({ texto: 'Nome inválido' })
+  }
+  if (!req.body.slug) {
+    erros.push({ texto: 'Slug inválido' })
+  }
+  if (req.body.nome.length < 2) {
+    erros.push({ texto: 'Nome da categoria muito curto' })
+  }
+  if (erros.length > 0) {
+    res.render('admin/addcategorias', { erros: erros })
+  } else {
+    const novaCategoria = {
+      nome: req.body.nome,
+      slug: req.body.slug
+    }
+    new Categoria(novaCategoria)
+      .save()
+      .then(() => {
+        req.flash('success_msg', 'Categoria criada com sucesso!')
+        res.redirect('/admin/categorias')
+      })
+      .catch(err => {
+        req.flash(
+          'error_msg',
+          'Houve um erro ao salvar a categoria, tente novamente.'
+        )
+        res.redirect('/admin')
+      })
+  }
 })
 
 module.exports = router
